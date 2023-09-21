9ea9b98696ae8fc4f80051c8b66cc330b1b39add
Andres <95327624+scarfon@users.noreply.github.com>
2023-09-21 18:52:32 -0300

    add api

--- src/app/components/button.jsx
+++ src/app/components/button.jsx
@@ -1,7 +1,15 @@
-export function Button ({ text, icon }) {
-  return (
-    <button className="flex items-center justify-center bg-[#C1EDCC] text-black rounded-md px-4 py-2 mt-4">
-      <span className="mr-2">{text}</span>
-    </button>
-  );
-};
+import { useRouter } from "next/navigation";
+
+export function Button({ text, redirect }) {
+	const router = useRouter();
+	return (
+		<button
+			onClick={() => {
+				router.push(redirect);
+			}}
+			className="flex items-center justify-center bg-[#C1EDCC] text-black rounded-md px-4 py-2 mt-4"
+		>
+			<span className="mr-2">{text}</span>
+		</button>
+	);
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,26 @@
 import Image from "next/image";
 import { Card } from "./components/cardevento";
-let evento = {
-	title:"Evento 1",
-	imageSrc:"https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F530262349%2F268399181129%2F1%2Foriginal.20230606-143800?w=940&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C60%2C1920%2C960&s=22717efd92cc4325ada9b8b3c5cd012f",
-	boldText:"Sábado, 2 de setembro",
-	normalText:"Peter Pan",
-	buttonText:"Learn More",
-	buttonIcon:"arrow-right",
-	id:1
-}
 
-export default function Home() {
+async function carregarDados() {
+	const url = `https://api.seatgeek.com/2/events/?client_id=${process.env.NEXT_PUBLIC_ID}&client_secret=${process.env.NEXT_PUBLIC_SECRET}`;
+	console.log(url);
+	const resposta = await fetch(url);
+	const dados = await resposta.json();
+	return dados.events;
+}
+let evento = {
+	title: "Evento 1",
+	imageSrc:
+		"https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F530262349%2F268399181129%2F1%2Foriginal.20230606-143800?w=940&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C60%2C1920%2C960&s=22717efd92cc4325ada9b8b3c5cd012f",
+	boldText: "Sábado, 2 de setembro",
+	normalText: "Peter Pan",
+	buttonText: "Learn More",
+	buttonIcon: "arrow-right",
+	id: 1,
+};
+
+export default async function Home() {
+	const eventos = await carregarDados();
 	return (
 		//JSX
 		<>
@@ -20,9 +30,12 @@ export default function Home() {
 				<ul className="flex space-x-4 self-center"> favoritos</ul>
 				<ul className="flex space-x-4 self-center"> carrinho</ul>
 			</nav>
-			<Card
-				evento={evento}
-			/>
+
+			<section className="flex flex-wrap gap-2">
+				{eventos.map((evento) => (
+					<Card evento={evento} />
+				))}
+			</section>
 		</>
 	);
 }
